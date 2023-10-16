55f17637ec1a3d33e2d5250288e1c3956e3ed506
Nasibb <nasibgi@code.edu.az>
2023-10-16 18:14:33 +0400

    first

---
+++ ALL TASK/Program.cs
@@ -0,0 +1,70 @@
+﻿#region 1.Verilmiş n və m (n<m) ədədləri arasında neçə tək ədəd olduğunu tapın.
+
+//int n = int.Parse(Console.ReadLine());
+//int m = int.Parse(Console.ReadLine());
+//int count = 0;
+//for (int i = n; i <= m; i++)
+//{
+//    if (i %2 == 1)
+//    {
+//        count++;
+//    }
+//}
+//Console.WriteLine(count);
+
+#endregion
+
+#region 1.1 Verilmiş n və m (n<m) ədədləri arasındakı cüt ədədlərin cəmini tapın.
+
+//int n=int.Parse(Console.ReadLine());
+//int m=int.Parse(Console.ReadLine());
+//int sum = 0;
+//for(int i=n;i<=m;i++)
+//{
+//    if(i%2==0)
+//    {
+//        sum += i;
+//    }
+//}
+//Console.WriteLine(sum);
+
+#endregion
+
+#region 1.2 Verilmiş n və m (n<m) ədədləri arasındakı tək ədədlərin hasilini tapın.
+
+//int n = int.Parse(Console.ReadLine());
+//int m = int.Parse(Console.ReadLine());
+//int multi = 1;
+//if (n > m)
+//{
+//    Console.WriteLine("diger eded daxil edin");
+//}
+//else if (n < m)
+//{
+//    for (int i=n; i <= m; i++)
+//    {
+//        if (i%2==1)
+//        {
+//            multi *= i;
+//        } 
+//    }
+//    Console.WriteLine(multi);
+//}
+
+
+#endregion
+
+#region 1.Verilmiş n və m (n<m) ədədləri arasında neçə ədəd olduğunu tapın.
+
+int n = int.Parse(Console.ReadLine());
+int m = int.Parse(Console.ReadLine());
+int count = 0;
+for (int i = n; i <= m; i++)
+{
+        count++;
+}
+Console.WriteLine(count);
+
+#endregion
+
+
